71734de1ede788c20c99de5be6e3eff4abafe531
Remon Nashid <remon.sherin@gmail.com>
2019-12-10 08:07:25 -0800

    Add polkadot.js and polymesh types

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 import logo from './logo.svg';
 import './App.css';
+import polymeshTypes from './polymeshTypes'
 
+const PROVIDER_URL = 'ws://78.47.58.121:9944'
 function App() {
+  const [api, setApi] = useState(null);
+
+  useEffect(() => {
+    async function connect() {
+      console.log('connecting')
+      const wsProvider = new WsProvider(PROVIDER_URL)
+      const api = await ApiPromise.create({
+        provider: wsProvider,
+        types: polymeshTypes
+      })
+      setApi(api)
+      global.api = api
+      console.log(api)
+    }
+    if (!api) {
+      connect()
+    }
+  }, [api])
+
   return (
     <div className="App">
       <header className="App-header">

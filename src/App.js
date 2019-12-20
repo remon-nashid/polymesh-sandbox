d99e5a637773e99dd66f0c0060c620cbe543eab6
Remon Nashid <remon.sherin@gmail.com>
2019-12-20 04:49:27 -0800

    Add token creation + attempt at reuse

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ApiPromise, WsProvider } from '@polkadot/api';
 import { web3Accounts, web3Enable, web3FromAddress } from '@polkadot/extension-dapp';
 import { stringToHex } from '@polkadot/util';
 import BN from 'bn.js';
+import useForm from 'react-hook-form';
 
 import logo from './logo.svg';
 import './App.css';
@@ -11,6 +12,7 @@ import polymeshTypes from './polymeshTypes.json'
 const PROVIDER_URL = 'wss://pmd.polymath.network'
 const EMPTY = '0x0'
 const ZERO = '0x0000000000000000000000000000000000000000000000000000000000000000'
+const ticker = stringToHex('MYTOKEN')
 
 function App() {
   const [address, setAddress] = useState(null);
@@ -18,6 +20,38 @@ function App() {
   const [did, setDid] = useState(null);
   const [nonce, setNonce] = useState(new BN(0));
   const [assets, setAssets] = useState(null)
+
+  const { register, handleSubmit, errors } = useForm(); // initialise the hook
+
+  async function _singAndSend(api, from) {
+    return (method, params) => {
+      new Promise(async (resolve, reject) => {
+        try {
+          const nonce = await api.query.system.accountNonce(address);
+          method(...params).signAndSend(from, {nonce}, ({ events = [], status }) => {
+            if (status.isFinalized) {
+              const relevantEvents = []
+              events = events.filter(({ phase, event: { data, method, section } }) => {
+                const { methodName, sectionName } = method().method.methodName
+                if(section === sectionName 
+                  // && 
+                  // method === methodName
+                  ) {
+                  return data
+                }
+              });
+              resolve(relevantEvents)
+            }
+          })
+        }
+        catch (error) {
+          reject(error)
+        }
+      });
+    }
+  }
+
+  const signAndSend = _singAndSend(api, address)
 
   useEffect(() => {
     async function connect() {
@@ -94,24 +128,25 @@ function App() {
   useEffect(() => {
     async function registerDid() {
       try {
-        await api.tx.identity
-          .registerDid([])
-          .signAndSend(address, {nonce}, ({ events = [], status }) => {
-            console.log('Transaction status:', status);
-            if (status.isFinalized) {
-              events.forEach(({ phase, event: { data, method, section } }) => {
-                if(section === 'identity' && method === 'NewDid') {
-                  console.log('NewDid data', data.toString())
+        // await api.tx.identity
+        //   .registerDid([])
+        //   .signAndSend(address, {nonce}, ({ events = [], status }) => {
+        //     console.log('Transaction status:', status);
+        //     if (status.isFinalized) {
+        //       events.forEach(({ phase, event: { data, method, section } }) => {
+        //         if(section === 'identity' && method === 'NewDid') {
+        //           console.log('NewDid data', data.toString())
 
-                  const did = data[0].toString()
-                  setDid(did)
-                  setNonce(nonce.add(new BN(1)))
-                }
-              });
-              // setDid(ret)
-              setNonce(nonce.add(new BN(1)))
-            }
-          })
+        //           const did = data[0].toString()
+        //           setDid(did)
+        //           setNonce(nonce.add(new BN(1)))
+        //         }
+        //       });
+        //     }
+        //   })
+
+        const did = (await signAndSend(api.registerDid([])))[0].toString()
+        console.log(did)
       } 
       catch (error) {
         console.log('registerDid error', error)
@@ -127,7 +162,6 @@ function App() {
 
   useEffect(() => {
     async function fetchToken() {
-      const ticker = stringToHex('MYTOKEN')
       // const ticker = 'MyToken'
       // 0: {"name" => Bytes(50)}
       // 1: {"total_supply" => Type}
@@ -147,26 +181,50 @@ function App() {
     }
   }, [did])
 
-  // async function createToken() {
-  //   createToken(did, name, _ticker, total_supply, divisible)
-  //   .createToken(dids[i], 'ticker', ticker, 1000000, true)
-  // }
+  console.log('Form errors', errors)
+
+  async function createToken() {
+    let name = 'MYTOKEN', _ticker = 'MYTOKEN', total_supply = 1000, divisible = true
+    name = stringToHex(name)
+    _ticker = stringToHex(_ticker)
+    total_supply = new BN(total_supply)
+    console.log(did, name, _ticker, total_supply, divisible)
+
+    api.tx.asset.createToken(did, name, _ticker, total_supply, divisible)
+      .signAndSend(address, {nonce}, ({ events = [], status }) => {
+        console.log('Transaction status:', status);
+        if (status.isFinalized) {
+          events.forEach(({ phase, event: { data, method, section } }) => {
+            // if(section === 'asset' && method === 'NewDid') {
+              
+            console.log('CreatedToken', method, section, data.toString())
+            // }
+          });
+          setNonce(nonce.add(new BN(1)))
+        }
+      })
+  }
+
+  const onSubmit = data => {
+    // @TODO send data
+    createToken()
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>Ticker</label>
+          <input name="_ticker" ref={register} />
+          <label>Name</label>
+          <input name="name" ref={register} />
+          <label>Total supply</label>
+          <input name="total_supply" ref={register} />
+          <label>Divisible</label>
+          <input name="divisible" ref={register} />
+          <input type="submit" onClick={handleSubmit(onSubmit)} />
+        </form>
       </header>
     </div>
   );
